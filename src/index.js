b4e556e620a2456c2ae2d8dc8b54e62aa6f86bb0
hgatayaandf <121156667+hgatayaandf@users.noreply.github.com>
2025-01-05 08:48:32 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -29,4 +29,4 @@ const gracefulShutdown = () => {
 
 process.on('SIGINT', gracefulShutdown);
 process.on('SIGTERM', gracefulShutdown);
-process.on('SIGUSR2', gracefulShutdown); // Sent by nodemons
+process.on('SIGUSR2', gracefulShutdown); // Sent by nodemon
